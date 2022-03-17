6fb433ef4e03f0395fae304c9e409efa45de067c
skokhatska <fCfjMpSKmF7w>
2022-03-17 22:31:04 +0400

    Publisher Relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -23,28 +23,35 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        Publisher publisher = new Publisher("Address line", "New York", "New York", "1234");
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Novel");
         Book newBook = new Book("New Desighn", "123123123");
         eric.getBooks().add(newBook);
         newBook.getAuthors().add(eric);
+        newBook.setPublisher(publisher);
+        publisher.getBooks().add(newBook);
 
         authorRepository.save(eric);
         bookRepository.save(newBook);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Williams");
         Book newBook2 = new Book("BOOOk", "789343450");
         rod.getBooks().add(newBook2);
         newBook2.getAuthors().add(rod);
+        newBook2.setPublisher(publisher);
+        publisher.getBooks().add(newBook2);
 
         authorRepository.save(rod);
         bookRepository.save(newBook2);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in BootStrap");
         System.out.println("Number of books: " + bookRepository.count());
-
-        Publisher publisher = new Publisher("Address line", "New York", "New York", "1234");
-        publisherRepository.save(publisher);
         System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Number of publishers books: " + publisher.getBooks().size());
 
     }
 }
